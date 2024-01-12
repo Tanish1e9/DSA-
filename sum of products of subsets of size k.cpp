8b2fbd4eecfa5aae3a73c16d852b74e8df33ab34
Tanish1e9 <156431287+Tanish1e9@users.noreply.github.com>
2024-01-13 02:24:09 +0530

    Update sum of products of subsets of size k.cpp

--- sum of products of subsets of size k.cpp
+++ sum of products of subsets of size k.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 long f(int i,int k,vector<int>&arr,auto &memo){
 	if(i+1<k) return 0;
-	if(k==1) {
-		long sum=0;
-		for(int j=0;j<=i;j++){
-			sum += arr[j];
-		}
-		return sum;
-	}
+	// if(k==1) {
+	// 	long sum=0;
+	// 	for(int j=0;j<=i;j++){
+	// 		sum += arr[j];
+	// 	}
+	// 	return sum;
+	// }
+	if(k==0) return 0;
 	if(memo[i][k]!=-1) return memo[i][k];
-	long pick = arr[i] * f(i-1,k-1,arr,memo);
+	long pick = arr[i];
+	if(k>1) pick *= f(i-1,k-1,arr,memo);
 	long npick = f(i-1,k,arr,memo);
 	return memo[i][k]=pick + npick;
 }
@@ -19,19 +21,19 @@ int main(){
 	vector<int> arr;
 	for(int i=1;i<=1e3;i++) arr.push_back(i);
 	int k=3;
-	// vector<vector<long>> memo(arr.size(),vector<long>(k+1,-1));
-	// cout<<f(arr.size()-1,k,arr,memo)<<endl;
-	vector<long> prev(k+1,0);
-	prev[0] = 1;
-	prev[1] = arr[0];
-	for(int i=1;i<arr.size();i++){
-		for(int x=k;x>=1;x--){
-			if(i+1<x) prev[x]=0;
-			long pick = arr[i] * prev[x-1];
-			long npick=0;
-			if(i+1>=x) npick = prev[x];
-			prev[x]=pick + npick;
-		}
-	}
-	cout<<prev[k];
+	vector<vector<long>> memo(arr.size(),vector<long>(k+1,-1));
+	cout<<f(arr.size()-1,k,arr,memo)<<endl;
+	// vector<long> prev(k+1,0);
+	// prev[1] = arr[0];
+	// for(int i=1;i<arr.size();i++){
+	// 	for(int x=k;x>=1;x--){
+	// 		if(i+1<x) prev[x]=0;
+	// 		long pick = arr[i] ;
+	// 		if(x>1) pick = pick *prev[x-1];
+	// 		long npick=0;
+	// 		if(i+1>=x) npick = prev[x];
+	// 		prev[x]=pick + npick;
+	// 	}
+	// }
+	// cout<<prev[k];
 }
